dd42551ccfd76b79b58e6de9bb279ff043119d0d
cheetahzlesa <151737413+cheetahzlesa@users.noreply.github.com>
2024-06-09 17:33:58 +0200

    Update find_rezistor.py

--- power_suply_shc_1/find_rezistor/find_rezistor.py
+++ power_suply_shc_1/find_rezistor/find_rezistor.py
@@ -1,6 +1,7 @@
 cnt = 0
 rezistors = dict()
 out = 0.8
+min, max = 3.25, 3.4
 with open("series.txt", "r")as file:
     for riadok in file:
         if cnt%2 == 0:
@@ -11,12 +12,12 @@ with open("series.txt", "r")as file:
                 if item not in rezistors.keys():
                     rezistors[item] = serie
         cnt += 1
-print(rezistors.items())
+#print(rezistors.items())
 
 with open("result.csv", "w")as output:
     output.write("R1;Serie1;R2;Serie2;Source\n")
     for r1 in rezistors.keys():
         for r2 in rezistors.keys():
             source = (out*(r1+r2))/r2
-            if (2 <= source <= 3.4):
+            if (min <= source <= max):
                 output.write(f'{r1};{rezistors[r1]};{r2};{rezistors[r2]};{source}\n')
