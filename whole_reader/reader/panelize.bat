12daa83a2a042dfeec1be965a5017fba81854a65
Richard Ostertág <rios@roster.sk>
2025-04-23 08:25:14 +0200

    Oprava skriptu a zmena panela na 3x2

--- whole_reader/reader/panelize.bat
+++ whole_reader/reader/panelize.bat
@@ -2,4 +2,4 @@ REM Obsah adresára je možné vymazať. Celý sa generuje týmto skriptom.
 REM Na aktualizáciu panelu stačí tento skript znovu spustiť.
 
 kikit panelize -p jlcpcb-reader.json reader.kicad_pcb panel\panel.kicad_pcb
-kikit fab jlcpcb --schematic reader.kicad_sch panel.kicad_pcb panel\
+kikit fab jlcpcb --schematic reader.kicad_sch panel\panel.kicad_pcb panel\
