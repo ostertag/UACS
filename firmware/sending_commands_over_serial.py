55bdbd2e2049dcd6a81d168f43d2d194ac63e13b
cheetahzlesa <151737413+cheetahzlesa@users.noreply.github.com>
2025-06-01 00:57:45 +0200

    Update sending_commands_over_serial.py

--- firmware/sending_commands_over_serial.py
+++ firmware/sending_commands_over_serial.py
@@ -7,38 +7,38 @@ class SendCommandsOverSerial:
 
     def blick_all_diodes(self, number_of_cycles):
         print("blick_all_diodes")
-        self.ser.write(f"LB-{number_of_cycles}".encode())
+        #self.ser.write(f"LB-{number_of_cycles}".encode())
     
     def togle_diode(self, diode : str):
         print("togle_diode")
-        self.ser.write(f"L-{diode}".encode())
+        #self.ser.write(f"L-{diode}".encode())
     
 
     def start_pwm(self, frequency):
         print("start_pwm")
-        self.ser.write(f"T-{frequency}".encode())
+        #self.ser.write(f"T-{frequency}".encode())
 
     def stop_pwm(self):
         print("stop_pwm")
-        self.ser.write(f"TS".encode())
+        #self.ser.write(f"TS".encode())
         
 
     def start_uart_transmision(self, payload : str):
         print("start_uart_transmision")
-        self.ser.write(f"U-{payload}".encode())
+        #self.ser.write(f"U-{payload}".encode())
     
     def stop_uart_transmision(self):
         print("stop_uart_transmision")
-        self.ser.write(f"US".encode())
+        #self.ser.write(f"US".encode())
         
 
     def receive_from_cdc(self)-> str:
         print("receive_from_cdcs")
-        return_str = ""
-        while return_str == "":
-            return_str = self.ser.readline().decode("utf-8").strip
-        
-        print(return_str)
+        #return_str = ""
+        #while return_str == "":
+        #    return_str = self.ser.readline().decode("utf-8").strip
+        #
+        #print(return_str)
 
     def send_command(self, func, param = None):
         if param == None:
